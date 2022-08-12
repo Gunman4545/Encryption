83ff66acac587e1abc09c778caf377dd7d5235b0
Puru Kaushik <kaushik.swapnil5@gmail.com>
2022-08-12 18:20:56 +0200

    Adding record audio as its on promise

--- Source/public/otp_index.js
+++ Source/public/otp_index.js
@@ -1,21 +1,37 @@
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(stream => {
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     const mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.start();
-
     const audioChunks = [];
+
     mediaRecorder.addEventListener("dataavailable", event => {
       audioChunks.push(event.data);
     });
 
-    mediaRecorder.addEventListener("stop", () => {
-      const audioBlob = new Blob(audioChunks);
-      const audioUrl = URL.createObjectURL(audioBlob);
-      const audio = new Audio(audioUrl);
-      audio.play();
-    });
+    const start = () => mediaRecorder.start();
 
-    setTimeout(() => {
-      mediaRecorder.stop();
-    }, 3000);
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
   });
+
+  (async () => {
+    const recorder = await recordAudio();
+    recorder.start();
+  
+    setTimeout(async () => {
+      const audio = await recorder.stop();
+      audio.play();
+    }, 3000);
+  })();
